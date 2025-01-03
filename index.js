359731d08e66c69c43e874112a2de17e3ca485d9
srshakil2 <rh3642921@gmail.com>
2025-01-03 18:53:02 +0600

    addmarathon and my merathon get update delete done

--- index.js
+++ index.js
@@ -38,17 +38,37 @@ async function run() {
       const result = await users.toArray();
       res.send(result);
     });
+    // home page limit data
+    app.get("/sixData", async (req, res) => {
+      const users = dataCalection.find().limit(6);
+      const result = await users.toArray();
+      res.send(result);
+    });
     app.get("/data", async (req, res) => {
       const users = dataCalection.find();
       const result = await users.toArray();
       res.send(result);
     });
+    //
     app.get("/data/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await dataCalection.findOne(query);
       res.send(result);
     });
+    // todo
+    app.get("/data/singeldata/:email", async (req, res) => {
+      const email = req.params.email;
+      // console.log(email);
+      const query = { email: email };
+      // console.log(query);
+      const result = await dataCalection.find(query).toArray();
+      // const query = { _id: new ObjectId(id) };
+      // const result = await dataCalection.findOne(query);
+      res.send(result);
+    });
+
+    //
     app.get("/participer", async (req, res) => {
       const participers = participerColection.find();
       const result = await participers.toArray();
@@ -62,6 +82,13 @@ async function run() {
       res.send(result);
     });
     //
+    // data post
+    app.post("/data", async (req, res) => {
+      const newData = req.body;
+      // console.log(newData);
+      const result = await dataCalection.insertOne(newData);
+      res.send(result);
+    });
     app.post("/user", async (req, res) => {
       const newUser = req.body;
       const result = await marathonUserCalection.insertOne(newUser);
@@ -100,12 +127,41 @@ async function run() {
       const result = await participerColection.deleteOne(queryId);
       res.send(result);
     });
+
+    // deleteOne datacalection
+    app.delete("/data/deleteOne/:id", async (req, res) => {
+      const data = req.params.id;
+      const query = { _id: new ObjectId(data) };
+      console.log(data, query);
+      const result = await dataCalection.deleteOne(query);
+      res.send(result);
+    });
+    // datacalection update
+    app.patch("/data/upDate/:id", async (req, res) => {
+      const data = req.body;
+      // console.log(data?.id);
+      const query = { _id: new ObjectId(data.id) };
+      // console.log(query);
+      const upDateDoc = {
+        $set: {
+          marathon_title: data.marathon_title,
+          marathon_image: data.marathon_image,
+          location: data.location,
+          start_registration_date: data.start_registration_date,
+          end_registration_date: data.end_registration_date,
+          description: data.description,
+        },
+      };
+      // console.log(upDateDoc);
+      const result = await dataCalection.updateOne(query, upDateDoc);
+      res.send(result);
+    });
     // incriment counter
     app.post("/data/:id", async (req, res) => {
       const updatedCount = req.body;
       const query = { _id: new ObjectId(updatedCount?._id) };
       const filter = await dataCalection.findOne(query);
-      console.log(filter);
+      // console.log(filter);
       let total_registration_count = 0;
       if (filter?.total_registration_count) {
         total_registration_count = filter?.total_registration_count + 1;
@@ -118,7 +174,7 @@ async function run() {
           register_email: updatedCount?.email,
         },
       };
-      console.log(updatedCount, total_registration_count);
+      // console.log(updatedCount, total_registration_count);
       const result = await dataCalection.updateOne(query, updatedDoc);
       res.send(result);
     });
